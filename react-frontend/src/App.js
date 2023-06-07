426fd79adb13833bb71061161b7c92c0b6231a66
EllisChang97 <ellischang99@gmail.com>
2023-06-07 11:49:07 -0400

    adding frontend React files to the repo

---
+++ react-frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Body from './components/Body';
+
+// Pixy camera dimensions
+// W:316
+// H:208
+
+function App() {
+  return (
+    <div className="App">
+        <Body />      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-frontend/src/components/Body.jsx
@@ -0,0 +1,11 @@
+export default function Body() {
+    return (
+        <div id="header">
+            <div id="map">
+                <img id="cityImage" src="/images/lego_city.webp"></img>
+                <div id='car-1'></div>
+            </div>
+        </div>
+        
+    )
+}
