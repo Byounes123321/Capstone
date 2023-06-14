65b02e8c6c99d004906166418dc3ccf5c7ed1474
EllisChang97 <ellischang99@gmail.com>
2023-06-14 16:36:20 -0400

    added proper image and moving cars

--- react-frontend/src/components/CarMap.jsx
+++ react-frontend/src/components/CarMap.jsx
@@ -1,9 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
 export default function CarMap() {
+    
+    // Define the state variable for the top value
+    const [car1ycord, setcar1ycord] = useState('0px');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setcar1ycord(Math.random() * 306 + 'px');
+        }, 1000);
+
+        // Clean up the interval when the component is unmounted
+        return () => clearInterval(interval);
+        }, []);
+
     return (
         <div id="map">
-                <img id="cityImage" src="/images/lego_city.webp"></img>
-                <div id='car-1'></div>
-                <p>hello</p>
+                <img id="cityImage" src="/images/legoCity.png"></img>
+                <div id='car-1' style={{top: car1ycord, left: '110px'}}><p class="carLabel">1</p></div>
+                <div id='car-2' style={{top: car1ycord, left: '360px'}}><p class="carLabel">2</p></div>
+                <p>Pink Car y-coordinate: {car1ycord}</p>
         </div>
     )
 }
