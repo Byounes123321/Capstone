2a2e0e6afbe89fb89a396fe0f8c8ab18771c792f
EllisChang97 <ellischang99@gmail.com>
2023-06-21 09:34:30 -0400

    commit

--- react-frontend/src/components/Body.jsx
+++ react-frontend/src/components/Body.jsx
@@ -1,11 +1,20 @@
-import UselessFact from "./uselessFact";
 import CarMap from "./CarMap";
 
 export default function Body() {
     return (
-        <div id="header">
-            <CarMap />
-            
+        <div>
+            <header>
+                <div id="header_contents">
+                    <img id="brickmmoLogo" src="/images/brickmmoLogo.jpg" />
+                    <h1>GPS</h1>
+                </div>
+            </header>
+            <div id="carMap">
+                <CarMap />
+            </div>
+            <footer>
+                <p>&copy; Copyright BRICKMMO, 2023.</p>
+            </footer>
         </div>
         
     )
